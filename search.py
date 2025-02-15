0e03251357e4b93d7464668313cf71dbbbfcdb99
Kevin Zong <7kevinzong@gmail.com>
2025-02-15 13:51:14 -0500

    search function implement ENPM611

--- search.py
+++ search.py
@@ -5,4 +5,9 @@ def search_json(json_data, search_string):
     # Place your search code here
     # you will have to loop through the json_data file you created earlier
     # finally you can store the match in the result list and return it
+    for item in json_data:
+        user = item.get("User", "Unknown user")
+        rating = item.get(search_string, "N/A")
+        results.append(f"{user}: {rating}")
+
     return results
